fa73c6f700929f2c27d053b85bd3799cf51c8676
Farzan Khoobchehr <farzan_khoobchehr@yahoo.com>
2019-05-26 18:24:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,23 @@
 # code your #position_taken? method here!
 
+<<<<<<< HEAD
 
 def position_taken?(board, index)
   ((board[index] != " ") && (board[index] != "") && (board[index] != nil))
 end
+=======
+board = [" ", " ", " ", " ", " ", " ", " "]
+
+def position_taken?(zamin, index)
+  
+  if zamin[index] == " " || zamin[index] == "" || zamin[index] == nil
+    false
+    
+  else
+    true
+    
+end
+
+
+position_taken?(board, input)
+>>>>>>> b2e89c6e98dc5c3e394cb0faab585d45841889ad
